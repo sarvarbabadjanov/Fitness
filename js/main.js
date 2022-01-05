f4246ca64e45e12e227262a00806ee74b0e9cab6
sarvarbabadjanov <97178499+sarvarbabadjanov@users.noreply.github.com>
2022-01-05 20:15:05 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  
+var owl = $('.owl-carousel').owlCarousel({
+        items: 1,
+        loop: true,
+        pagination : true,
+        nav: true, 
+        dots: true,
+        autoplay: false,
+        smartSpeed: 2000,
+        autoplayTimeout: 5000
+    });
+
+});
